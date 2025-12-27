43f0f98b9dd308e28b698155661c876ae05369bb
chill-coderGuy <sbhatia2711@gmail.com>
2025-12-27 19:57:51 +0530

    fix: reorganised the code

--- CurveGen_dataset.py
+++ CurveGen_dataset.py
@@ -5,9 +5,64 @@ def test_data(noise=0.0):
     test_data_max = 10
     test_values = 100
     x_test_data = np.linspace(test_data_min, test_data_max, test_values)    
-    y_test_data = x_test_data**2+np.sin(x_test_data)
+    y_test_data = np.exp(x_test_data*2)
     
     if noise > 0:
         y_test_data += np.random.normal(0, noise, size=len(y_test_data))
 
     return x_test_data, y_test_data
+
+def build_baseTerms(x):
+    x_clean=np.abs(x)+1e-9
+    func_dict = {
+        "1":      np.ones_like(x),
+        "x":      x,
+        "x^2":    x**2,
+        "1/x":    1.0/x_clean,
+        "sqrt":   np.sqrt(x),
+        "sin":    np.sin(x),
+        "cos":    np.cos(x),
+        "exp":    np.exp(x),
+        "log":    np.log(x_clean),
+        "1/exp":  np.exp(x*(-1)),
+        "1/exp2": np.exp(x*x*(-1))
+    }
+    
+    term_name = []
+    term_valv = []
+    keys = list(func_dict.keys())
+
+    for i in range(len(keys)):
+        for j in range(i, len(keys)):
+            k1 = keys[i]
+            k2 = keys[j]
+            
+            if k1 == "1" and k2 == "1": name,val= "1",func_dict['1']
+            if k1 == "1":    name, val = k2, func_dict[k2]
+            elif k2 == "1":  name, val = k1, func_dict[k1]
+            elif k1=="x" and k2=="1/x": continue
+            elif k1=="x" and k2=="1/x": continue
+            elif k1=="exp" and k2=="1/exp": continue
+            elif k1=="1/exp" and k2=="1/exp": continue
+            elif k1=="1/exp" and k2=="1/exp2": continue
+            elif k1=="1/exp2" and k2=="1/exp2": continue
+            elif k1=="exp" and k2=="1/exp2": continue
+            elif k1=="sqrt" and k2=="sqrt": continue
+            elif k1=="1/x" and k2=="1/x": continue
+            elif k1=="cos" and k2=="log": continue
+            elif k1=="sin" and k2=="log": continue
+            elif k1=="sqrt" and k2=="log": continue
+            elif k1=="sqrt" and k2=="exp": continue
+            elif k1=="sqrt" and k2=="sin": continue
+            elif k1=="sqrt" and k2=="cos": continue
+            elif k1=="log" and k2=="log": continue
+            elif k1=="x" and k2=="x": continue
+            elif k1=="x^2" and k2=="1/x": continue
+            else: name, val = f"{k1}*{k2}", func_dict[k1] * func_dict[k2]
+            
+            if name not in term_name:
+                term_name.append(name)
+                term_valv.append(val)
+    print(term_name)
+    return term_name, term_valv
+
--- CurveGen_greedy_algo.py
+++ CurveGen_greedy_algo.py
@@ -1,47 +1,8 @@
-import CurveGen_dataset
 import numpy as np
-from CurveGen_dataset import test_data
+from CurveGen_dataset import test_data,build_baseTerms
 
 x_data, y_data = test_data(noise=0.5) 
-def build_baseTerms(x):
-    x_clean=np.abs(x)+1e-9
-    func_dict = {
-        "1":      np.ones_like(x),
-        "x":      x,
-        "x^2":    x**2,
-        "1/x":    1.0/x_clean,
-        "sin":    np.sin(x),
-        "cos":    np.cos(x),
-        "exp":    np.exp(x),
-        "log":    np.log(x_clean),
-        "1/exp":  np.exp(x*(-1)),
-        "1/exp2": np.exp(x*x*(-1))
-    }
-    
-    term_name = []
-    term_valv = []
-    keys = list(func_dict.keys())
 
-    for i in range(len(keys)):
-        for j in range(i, len(keys)):
-            k1 = keys[i]
-            k2 = keys[j]
-            
-            if k1 == "1" and k2 == "1": name,val= "1",func_dict['1']
-            if k1 == "1":    name, val = k2, func_dict[k2]
-            elif k2 == "1":  name, val = k1, func_dict[k1]
-            elif k1=="x" and k2=="1/x": continue
-            elif k1=="x" and k2=="1/x": continue
-            elif k1=="exp" and k2=="1/exp": continue
-            elif k1=="x" and k2=="x": continue
-            elif k1=="x^2" and k2=="1/x": continue
-            else: name, val = f"{k1}*{k2}", func_dict[k1] * func_dict[k2]
-            
-            if name not in term_name:
-                term_name.append(name)
-                term_valv.append(val)
-    
-    return term_name, term_valv
 
 def greedy_algo(x, y):
     
@@ -95,23 +56,3 @@ coeffs, terms = greedy_algo(x_data, y_data)
 parts = [f"{c:.3f}[{t}]" for c, t in zip(coeffs, terms)]
 print(" + ".join(parts))
 
-def reduction_function(x,y):
-    term_name,term_valv=build_baseTerms(x)
-    term_name=np.array(term_name)
-    dominant_mask=[]
-    possible_dominants=[]
-    possible_dominants_r_value=[]
-    r_values=np.zeros(len(term_name))
-
-    for i in range(len(term_name)):
-        r_values[i]=np.corrcoef(y,term_valv[i])[0,1]
-    dominant_mask=r_values>0.95
-    possible_dominants=term_name[dominant_mask]
-    possible_dominants_r_value=r_values[dominant_mask]
-    print(possible_dominants)
-    print(possible_dominants_r_value)
-
-        
-   
-
-reduction_function(x_data,y_data)
