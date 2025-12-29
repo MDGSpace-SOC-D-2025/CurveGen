feat: added a input for dimension and obtain the unit from it,fix: changed the error metric from variance to mse

CurveGen_dataset.py
@@ -1,12 +1,31 @@
 import numpy as np
 
+unit_list=['kg','m','s','A','K']
+unit_dimensions={(1.0,):'Mass',(0,1):'Length'}
 def test_data(noise=0.0):
+    def get_dimensions(a="text"):
+        dimensions = np.array(input(a).split(), dtype=float)
+        return dimensions
+        
+    """
     test_data_min = 1
     test_data_max = 10
     test_values = 100
     x_test_data = np.linspace(test_data_min, test_data_max, test_values)    
-    y_test_data = np.exp(x_test_data*2)
-    
+    y_test_data = np.exp(x_test_data*2)+x_test_data*x_test_data+x_test_data+0.8
+    """
+    x_test_data = np.array(input("Enter the values of one of the parameters: ").split(), dtype=float)
+    x_dimensions=get_dimensions("input the unit for the parameter in the form of M L T I K")
+    x_dimensions_tuple=tuple(x_dimensions.tolist())
+    parts = [f"{t}[{c:.3f}]" for c, t in zip(x_dimensions, unit_list)]
+    print(f'Physical quantity is {unit_dimensions[x_dimensions_tuple]},'+' unit:'+"".join(parts))
+
+    y_test_data = np.array(input("Enter result values: ").split(), dtype=float)
+    print(y_test_data)
+    y_dimensions=get_dimensions("input the unit for the results in the form of M L T I K")
+    y_dimensions_tuple=tuple(y_dimensions.tolist())
+    parts = [f"{t}[{c:.3f}]" for c, t in zip(y_dimensions, unit_list)]
+    print(f'Physical quantity is {unit_dimensions[y_dimensions_tuple]},'+' unit:'+"".join(parts))
     if noise > 0:
         y_test_data += np.random.normal(0, noise, size=len(y_test_data))
 
@@ -63,6 +82,6 @@ def build_baseTerms(x):
             if name not in term_name:
                 term_name.append(name)
                 term_valv.append(val)
-    print(term_name)
+
     return term_name, term_valv
 

CurveGen_greedy_algo.py
@@ -1,13 +1,17 @@
 import numpy as np
 from CurveGen_dataset import test_data,build_baseTerms
 
-x_data, y_data = test_data(noise=0.5) 
+x_data, y_data = test_data(noise=0) 
+
+def mse(residuals):
+    m=np.mean(residuals**2)
+    return m
 
 
 def greedy_algo(x, y):
     
     term_name,term_valv=build_baseTerms(x)
-    current_error = np.var(y) 
+    current_error = mse(y) 
     
     found_indices = []
     found_cols = []
@@ -24,7 +28,7 @@ def greedy_algo(x, y):
             
             if resid.size > 0:
                 y_pred = A_test @ coeffs
-                new_error = np.var(y - y_pred)
+                new_error = mse(y - y_pred)
             else:
                 new_error = 0.0
             improvement = (current_error - new_error) / current_error
@@ -41,7 +45,7 @@ def greedy_algo(x, y):
         A_accepted = np.column_stack(found_cols)
         coeffs, _, _, _ = np.linalg.lstsq(A_accepted, y, rcond=None)
         y_pred = A_accepted @ coeffs
-        current_error = np.var(y - y_pred)
+        current_error = mse(y - y_pred)
         if current_error < 1e-9: break
     if not found_indices: return [], []
     